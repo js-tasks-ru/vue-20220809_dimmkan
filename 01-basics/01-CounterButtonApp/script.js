e7352f59864320f33074ec25205e62215b3f799e
Dmitry Kanaev <dimmkan@mail.ru>
2022-08-12 19:26:43 +0300

    Basic - Counter Button App

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,8 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+createApp({
+  data() {
+    return { count: 0 };
+  },
+}).mount('#app');
