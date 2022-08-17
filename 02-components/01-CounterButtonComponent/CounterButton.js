d80ca55148d1bd0a7bdd212055e00f12737c63f0
Dmitry Kanaev <dimmkan@mail.ru>
2022-08-17 08:42:30 +0300

    Components - CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,24 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
+  emits: ['update:count'],
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
+  methods: {
+    clickButton() {
+      this.$emit('update:count', this.count + 1);
+    },
+  },
+
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button"
+    @click="clickButton()"
+  > {{ count }} </button>`,
 });
