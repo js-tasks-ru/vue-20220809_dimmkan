9d8651466cf61d9362315bd014ce1abd739dbe49
Dmitry Kanaev <dimmkan@mail.ru>
2022-08-28 15:40:26 +0300

    05-vue-router - Fix import

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -1,24 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import PageIndex from '../views/PageIndex.vue';
-import PageLogin from '../views/PageLogin.vue';
-import PageRegister from '../views/PageRegister.vue';
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
   routes: [
     {
       path: '/',
-      component: PageIndex,
+      component: () => import('../views/PageIndex'),
       name: 'index',
     },
     {
       path: '/login',
-      component: PageLogin,
+      component: () => import('../views/PageLogin'),
       name: 'login',
     },
     {
       path: '/register',
-      component: PageRegister,
+      component: () => import('../views/PageRegister'),
       name: 'register',
     },
   ],
